068483d86f2b4209c056e0a90062e831eb0e7b08
Dipok Roy <roydipok022@gmail.com>
2022-05-28 17:25:04 +0600

    Business Summary Page added

--- src/pages/Home/BusinessSummary.js
+++ src/pages/Home/BusinessSummary.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import useParts from "../../Hooks/useParts";
 import Loading from "../../sherd/Loading/Loading";
+import summary from "../../image/Banner/summary.jpg";
 
 const BusinessSummary = () => {
   const [parts] = useParts();
@@ -26,10 +27,36 @@ const BusinessSummary = () => {
     return <Loading></Loading>;
   }
   return (
-    <div>
-      <h1>parts: {parts?.length}</h1>
-      <h1>order: {orders?.length}</h1>
-      <h1>reviews: {reviews?.length}</h1>
+    <div
+      style={{
+        background: `url(${summary})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="hero min-h-[600px] w-full"
+    >
+      <div className="hero-overlay bg-opacity-50 bg-[#000]"></div>
+      <div className="text-center">
+        <h1 className="text-3xl text-white mb-4 font-bold">Business Summary</h1>
+        <div className="w-full h-auto flex justify-center items-center">
+          <div className="bg-white px-12 md:px-6 py-8 rounded-xl">
+            <div className="grid md:grid-cols-3 gap-7 ">
+              <div>
+                <h1 className="text-xl text-gray-600">Products</h1>
+                <h1 className="text-3xl font-bold mt-2">{parts?.length}</h1>
+              </div>
+              <div>
+                <h1 className="text-xl text-gray-600">Reviews</h1>
+                <h1 className="text-3xl font-bold mt-2">{reviews?.length}</h1>
+              </div>
+              <div>
+                <h1 className="text-xl text-gray-600">Total Orders</h1>
+                <h1 className="text-3xl font-bold mt-2">{orders?.length}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
